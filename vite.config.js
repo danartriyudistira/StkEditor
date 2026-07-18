849c158e9d775750cdbcdafdea6c6f86286fce4a
gapindang <rizqullah.zuhayr.2405336@students.um.ac.id>
2026-07-18 08:11:21 +0700

    Fix base path to relative for GitHub Pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       languageWorkers: ['editorWorkerService', 'typescript', 'json'],
     }),
   ],
-  base: '/editor/',
+  base: './',
   server: {
     fs: {
       allow: [
